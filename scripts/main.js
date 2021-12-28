024cc7ef35589c215c0716d31be967256200ae1c
denis82 <30268500+8989denis@users.noreply.github.com>
2021-12-28 20:00:03 +0100

    UPDATE LATEST

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,23 @@
-var licznik=1;
+var licznik=true,licznik2=true;
 function funkcja(){
     alert();
 }
 function licencje(){
-    if(licznik>=2){
+    if(licznik==false){
         document.querySelector('footer>span>p:first-child').innerHTML='Licencje';
-        licznik = 1;
+        licznik = true;
     }else{
         let zmienna = '<div style="font-family: sans-serif;"><p>Font license info</p><p> ## Font Awesome</p><p> Copyright (C) 2016 by Dave Gandy</p><p> Author:    Dave Gandy</p><p> License:   SIL ()</p><p> Homepage:  http://fortawesome.github.com/Font-Awesome/</p></div>';
         document.querySelector('footer>span>p:first-child').innerHTML=zmienna;
-        licznik++;
+        licznik=false;
+    }
+}
+function toogleKontakt(){
+    if(licznik2==false){
+        document.querySelector('body>form').style.display="none";
+        licznik2 = true;
+    }else{
+        document.querySelector('body>form').style.display="block";
+        licznik2=false;
     }
 }
